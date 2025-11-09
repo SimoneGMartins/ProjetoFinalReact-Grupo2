513a7480bea95627e2cdc543ee74dd1aae476fd9
AmandaLisboa-Ramos <amandalisboa.am@gmail.com>
2025-11-09 13:21:16 -0300

    [+]Adicionando toda parte de login, estilizações e componentes

--- src/screens/Login/login.jsx
+++ src/screens/Login/login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputEmail from "../../components/ui/InputEmail/InputEmail";
+import InputPassword from "../../components/ui/InputPassword/InputPassword";
+import styles from "./login.module.css";
 
 const Login = () => {
-  return (
-    <div>login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, password });
+  };
+  return (
+    <section className={styles["loginContainer"]}>
+      <h1>Blog Jardim Digital</h1>
+      <div className={styles["loginBox"]}>
+        <h2>Bem vindo</h2>
+        <form onSubmit={handleSubmit} className={styles["loginForm"]}>
+          <InputEmail
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <InputPassword
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div className={styles["containerLinks"]}>
+            <button type="submit" className={styles["buttonSubmit"]}>
+              Sign In
+            </button>
+            <div>
+            <a href="#">Criar Conta</a>
+            <a href="#">Esqueceu a senha?</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
