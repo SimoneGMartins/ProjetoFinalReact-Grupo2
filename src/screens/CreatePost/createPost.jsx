5aa1cb97f723886c66dc37664a7ad6b249e18a19
simone <sgmartins80@gmail.com>
2025-11-10 22:54:02 -0300

    refatorando

--- src/screens/CreatePost/createPost.jsx
+++ src/screens/CreatePost/createPost.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import axios from "axios";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 import { toast } from "react-toastify";
 import styles from "./createPost.module.css"
 
